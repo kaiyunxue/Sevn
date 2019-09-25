4412ffff5a656a82e2b08cad46a1e6bd549a2ffb
cnsunshine <1005481829@qq.com>
2019-09-25 16:47:22 +0800

    NetTool

---
+++ Scripts/Tools/HttpTool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HttpTool : MonoBehaviour
+{
+
+
+    public static IEnumerator HttpGet(string url)
+    {
+        Debug.Log("enter HttpGet");
+        var request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
+    }
+
+    public static IEnumerator HttpPost(string url, WWWForm postData)
+    {
+        Debug.Log("enter HttpPost");
+        var request = UnityWebRequest.Post(url, postData);
+        yield return request.SendWebRequest();
+        if (request.isHttpError || request.isNetworkError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
+    }
+}
---
+++ Scripts/Tools/NetService.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class NetService : MonoBehaviour
+{
+    public enum MSG_ID
+    {
+        MSG_ID_LOGIN = 1,
+        MSG_ID_TICK = 2,
+    };
+
+    // Use this for initialization
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(Tick());
+    }
+
+    IEnumerator Tick()
+    {
+        WWWForm vMessageList = new WWWForm();
+        vMessageList.AddField("iMessageID", (int)MSG_ID.MSG_ID_TICK);
+        while(true)
+        {
+            yield return HttpTool.HttpPost("http://127.0.0.1:8000/api/post", vMessageList);
+            yield return new WaitForSeconds((float)0.5);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static IEnumerator SendMessage(MSG_ID eMessageID, WWWForm vMessageList)
+    {
+        vMessageList.AddField("iMessageID", (int)eMessageID);
+        yield return HttpTool.HttpPost("http://127.0.0.1:8000/api/post", vMessageList);
+    }
+}
