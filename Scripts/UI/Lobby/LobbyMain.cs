dcbd7aa8f71ea86b03f7e3362930f549d153aeab
MartinMHT <867796986@qq.com>
2019-10-08 01:04:52 +0800

    飞天妹妹语音

--- Scripts/UI/Lobby/LobbyMain.cs
+++ Scripts/UI/Lobby/LobbyMain.cs
@@ -5,8 +5,27 @@ using UnityEngine.UI;
 
 public class LobbyMain : LobbyUIBase
 {
+    [Header("引用")]
+    public AudioSource audioSource;
+    [Header("配置")]
+    public AudioClip[] modelAudios;
+    public float firstIntervalSecond;
+    public float audioIntervalSecond;
     protected override void Start()
     {
         base.Start();
+        StartCoroutine(PlayModelAudio());
+    }
+    
+    private IEnumerator PlayModelAudio()
+    {
+        yield return new WaitForSeconds(firstIntervalSecond);
+        while (true)
+        {
+            int randIdx = Random.Range(0, modelAudios.Length);
+            audioSource.clip = modelAudios[randIdx];
+            audioSource.Play();
+            yield return new WaitForSeconds(audioIntervalSecond);
+        }
     }
 }
